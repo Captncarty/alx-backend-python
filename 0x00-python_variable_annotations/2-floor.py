8d2fbc61d840aa9067244e4c73ed640f96c88691
Kelvin <edithmukoro3@gmail.com>
2023-07-06 22:18:25 +0100

    tsk 03

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+'''A module that returns the whole number from a float input'''
+
+
+def floor(n: float) -> int:
+    '''a function that reduces a float to an integer'''
+    return int(n)
+
+
+if __name__ == '__main__':
+    import math
